b7ad468f099227d663f09ce5370a912150300726
benny <shenbinsc@qq.com>
2021-03-20 14:56:08 +0800

    checkin

--- SDKClientSharp/MyTuple.cs
+++ SDKClientSharp/MyTuple.cs
@@ -7,13 +7,15 @@ namespace SDKClientSharp
 {
     class MyTuple<T1, T2>
     {
+        private T1 _item1;
+        private T2 _item2;
         public MyTuple(T1 item1, T2 item2)
         {
-            Item1 = item1;
-            Item2 = item2;
+            _item1 = item1;
+            _item2 = item2;
         }
 
-        public T1 Item1 { get; }
-        public T2 Item2 { get; }
+        public T1 Item1 { get { return _item1; } }
+        public T2 Item2 { get { return _item2; } }
     }
 }
